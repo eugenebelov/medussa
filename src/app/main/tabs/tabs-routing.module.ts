126bf011446a922f075811cb2b2f52b4ffad96f5
Yevhen Bilov <ybilov@deloitte.com>
2020-05-23 19:02:53 +0300

    refactor a bit components and add dashboard

--- src/app/main/tabs/tabs-routing.module.ts
+++ src/app/main/tabs/tabs-routing.module.ts
@@ -8,16 +8,16 @@ const routes: Routes = [
     component: TabsPage,
     children: [
       {
-        path: 'tab1',
-        loadChildren: () => import('../tab1/tab1.module').then(m => m.Tab1PageModule)
+        path: 'home-tab',
+        loadChildren: () => import('../home-tab/home.module').then(m => m.HomePageModule)
       },
       {
-        path: 'tab2',
-        loadChildren: () => import('../tab2/tab2.module').then(m => m.Tab2PageModule)
+        path: 'calendar-tab',
+        loadChildren: () => import('../calendar-tab/tab2.module').then(m => m.Tab2PageModule)
       },
       {
-        path: 'tab3',
-        loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
+        path: 'profile-tab',
+        loadChildren: () => import('../profile-tab/tab3.module').then(m => m.Tab3PageModule)
       },
       {
         path: 'more',
@@ -25,7 +25,7 @@ const routes: Routes = [
       },
       {
         path: '',
-        redirectTo: '/tabs/tab1',
+        redirectTo: '/tabs/home-tab',
         pathMatch: 'full'
       }
     ]
